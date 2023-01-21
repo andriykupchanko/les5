043eb04f92016b44f0d66a61c416b2ded6e79497
Andriy <kuphankoandriy@gmail.com>
2023-01-21 13:27:47 +0200

    create obj and put info obj user information

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 "use strict";
+let numberOfFilms = +prompt("How wathch films ?", 1);
+
+let personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+const LastMovie = prompt("How wathch the last films ?", ""),
+    RatengMovie = prompt("How reteng 1-10 ?", "");
+
+personalMovieDb.movies[LastMovie] = RatengMovie;
+
+console.log(personalMovieDb);
