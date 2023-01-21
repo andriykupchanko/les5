f166c6508da00700213fe4679fb711e47747a504
Andriy <kuphankoandriy@gmail.com>
2023-01-21 14:38:49 +0200

    conditions (if else and switch case , ?)in js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-let numberOfFilms = +prompt("How wathch films ?", 1);
+/*let numberOfFilms = +prompt("How wathch films ?", 1);
 
 let personalMovieDb = {
     count: numberOfFilms,
@@ -9,8 +9,37 @@ let personalMovieDb = {
     private: false
 };
 const LastMovie = prompt("How wathch the last films ?", ""),
-    RatengMovie = prompt("How reteng 1-10 ?", "");
+    RatengMovie = prompt("How reteng 1-10 ?", ""); 
 
 personalMovieDb.movies[LastMovie] = RatengMovie;
 
-console.log(personalMovieDb);
+console.log(personalMovieDb);*/
+
+if (4 == 5) {
+    console.log("you lox");
+} else {
+    console.log("ok");
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log("Error!");
+} else if (num > 100) {
+    console.log("Hight olds.");
+} else {
+    console.log("Its Ok.");
+}
+(num === 50) ? console.log("Its Ok."): console.log("Error!");
+
+switch (num) {
+    case 49:
+        console.log("Error!");
+        break;
+    case 100:
+        console.log("Hight olds.");
+        break;
+    default:
+        console.log("Its Ok.");
+        break;
+}
